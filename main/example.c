110365201510dec12e79ea38cb83fa7162dbf7f8
Dj Kilgore <dj@115industries.com>
2018-09-18 17:16:14 -0400

    ref(example): example code now uses FreeRTOS tasks and queues

--- main/example.c
+++ main/example.c
@@ -14,41 +14,55 @@
 
 #include "max31856.h"
 
-max31856_cfg_t max31856;
+QueueHandle_t thermocouple_queue = NULL;
+
+void receiver_task(void *pvParameter) {
+    ESP_LOGI("task", "Created: thermocouple_task");
+    QueueHandle_t *thermocouple_queue = pvParameter;
+	
+	max31856_cfg_t max31856;
+    if(thermocouple_queue == NULL){
+	    ESP_LOGI("queue", "Not Ready: thermocouple_queue");
+        return;
+    }
+    while(1){
+        xQueueReceive(thermocouple_queue,&max31856,(TickType_t )(200/portTICK_PERIOD_MS)); 
+        printf("Struct Received on Queue:\nCold Junction: %0.2f\nTemperature: %0.2f\n", max31856.coldjunction_f, max31856.thermocouple_f);
+        vTaskDelay(500/portTICK_PERIOD_MS);
+    }
+}
 
 void thermocouple_task(void *pvParameter) {
-    while(1) {
+    ESP_LOGI("task", "Created: receiver_task");
+    QueueHandle_t *thermocouple_queue = pvParameter;
 
-        // You can return data directly
-        thermocouple_read_fault(&max31856);
-        float cj_temp = thermocouple_read_coldjunction(&max31856);
-        float tc_temp = thermocouple_read_temperature(&max31856);
-        printf("Cold Junction Temp (C): %.2f\n", cj_temp);
-        printf("Thermocouple Temp (C): %.2f\n", tc_temp);
+    max31856_cfg_t max31856 = max31856_init();
+    thermocouple_set_type(&max31856, MAX31856_TCTYPE_K);
 
-        // Or pull the most recent data from the struct when you need it
-        printf("Cold Junction Temp (F): %.2f\n", max31856.coldjunction_f);
-        printf("Thermocouple Temp (F): %.2f\n",  max31856.thermocouple_f);
-        printf("Fault Register: %02X\n",  max31856.fault);
-
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-
+    if(thermocouple_queue == NULL){
+	    ESP_LOGI("queue", "Not Ready: thermocouple_queue");
+        return;
+    }
+    while(1){
+        thermocouple_read_fault(&max31856, true);
+        thermocouple_read_coldjunction(&max31856);
+        thermocouple_read_temperature(&max31856);
+        xQueueSend(thermocouple_queue,&max31856,(TickType_t )0);
+        vTaskDelay(200/portTICK_PERIOD_MS);
     }
 }
 
 void app_main() {
 
-    // Init SPI Device
-    max31856 = max31856_init();
-
-    // Set and Check Thermocouple Type
-    thermocouple_set_type(&max31856, MAX31856_TCTYPE_K);
-    thermocouple_get_type(&max31856);
-
-    // Set Temperature Fault Limits
-    //thermocouple_set_temperature_fault(&max31856, 0.0, 1300.0);
-
-    // Start Task to Get Temperature and Check Faults
-    xTaskCreate(&thermocouple_task, "thermocouple_task", 2048, NULL, 5, NULL);
+    // Queue: Thermocouple Data -> Receiver
+    thermocouple_queue = xQueueCreate(5, sizeof(struct max31856_cfg_t));
+    if(thermocouple_queue != NULL){
+	    ESP_LOGI("queue", "Created: thermocouple_queue");
+        vTaskDelay(1000/portTICK_PERIOD_MS);
+        xTaskCreate(&thermocouple_task, "thermocouple_task", 2048, (void *)thermocouple_queue, 5, NULL);
+        xTaskCreate(&receiver_task, "receiver_task", 2048, (void *)thermocouple_queue, 5, NULL);
+    } else{
+	    ESP_LOGI("queue", "Failed to Create: thermocouple_queue");
+    }  
     
 }
